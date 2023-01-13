342df1ac97c06d2ef6cd4dcbed1f4de48a773f16
Klavs Skovby <k.skovby@outlook.com>
2023-01-13 10:16:28 +0100

    New frontpage

--- PdfSharpCore/root/ProductVersionInfo.cs
+++ PdfSharpCore/root/ProductVersionInfo.cs
@@ -39,38 +39,38 @@ namespace PdfSharpCore
         /// <summary>
         /// The title of the product.
         /// </summary>
-        public const string Title = "PDFsharp";
+        public const string Title = "KlaskManager";
 
         /// <summary>
         /// A characteristic description of the product.
         /// </summary>
-        public const string Description = "A .NET library for processing PDF.";
+        public const string Description = "KLAVS@Ramboll.dk";
 
         /// <summary>
         /// The PDF producer information string.
         /// TODO: Called Creator in MigraDoc???
         /// </summary>
-        public const string Producer = Title + " " + VersionMajor + "." + VersionMinor + "." + VersionBuild + Technology + " (" + Url + ")";
+        public const string Producer = "";
 
         /// <summary>
         /// The PDF producer information string including VersionPatch.
         /// </summary>
-        public const string Producer2 = Title + " " + VersionMajor + "." + VersionMinor + "." + VersionBuild + "." + VersionPatch + Technology + " (" + Url + ")";
+        public const string Producer2 = "";
 
         /// <summary>
         /// The full version number.
         /// </summary>
-        public const string Version = VersionMajor + "." + VersionMinor + "." + VersionBuild + "." + VersionPatch;
+        public const string Version = "";
 
         /// <summary>
         /// The full version string.
         /// </summary>
-        public const string Version2 = VersionMajor + "." + VersionMinor + "." + VersionBuild + "." + VersionPatch + Technology;
+        public const string Version2 = "";
 
         /// <summary>
         /// The home page of this product.
         /// </summary>
-        public const string Url = "https://github.com/ststeiger/PdfSharpCore";
+        public const string Url = "";
 
         /// <summary>
         /// Unused.
@@ -80,22 +80,22 @@ namespace PdfSharpCore
         /// <summary>
         /// The company that created/owned the product.
         /// </summary>
-        public const string Company = "empira Software GmbH, Cologne Area (Germany)";
+        public const string Company = "";
 
         /// <summary>
         /// The name the product.
         /// </summary>
-        public const string Product = "PDFsharp";
+        public const string Product = "";
 
         /// <summary>
         /// The copyright information.
         /// </summary>
-        public const string Copyright = "Copyright © 2005-2016 empira Software GmbH.";
+        public const string Copyright = "";
 
         /// <summary>
         /// The trademark the product.
         /// </summary>
-        public const string Trademark = "PDFsharp";
+        public const string Trademark = "";
 
         /// <summary>
         /// Unused.
@@ -105,27 +105,27 @@ namespace PdfSharpCore
         /// <summary>
         /// The major version number of the product.
         /// </summary>
-        public const string VersionMajor = "1";
+        public const string VersionMajor = "";
 
         /// <summary>
         /// The minor version number of the product.
         /// </summary>
-        public const string VersionMinor = "50";
+        public const string VersionMinor = "";
 
         /// <summary>
         /// The build number of the product.
         /// </summary>
-        public const string VersionBuild = "4000";  // V16G // Build = days since 2005-01-01  -  change this values ONLY HERE
+        public const string VersionBuild = "";  // V16G // Build = days since 2005-01-01  -  change this values ONLY HERE
 
         /// <summary>
         /// The patch number of the product.
         /// </summary>
-        public const string VersionPatch = "0";
+        public const string VersionPatch = "";
 
         /// <summary>
         /// The Version Prerelease String for NuGet.
         /// </summary>
-        public const string VersionPrerelease = "beta3b"; // "" for stable Release, e.g. "beta" or "rc.1.2" for Prerelease. // Also used for NuGet Version.
+        public const string VersionPrerelease = ""; // "" for stable Release, e.g. "beta" or "rc.1.2" for Prerelease. // Also used for NuGet Version.
 
 #if DEBUG
         /// <summary>
@@ -139,7 +139,7 @@ namespace PdfSharpCore
         /// <summary>
         /// E.g. "2005-01-01", for use in NuGet Script.
         /// </summary>
-        public const string VersionReferenceDate = "2005-01-01";
+        public const string VersionReferenceDate = "";
 
         /// <summary>
         /// Use _ instead of blanks and special characters. Can be complemented with a suffix in the NuGet Script.
@@ -149,31 +149,31 @@ namespace PdfSharpCore
         /// or characters that are invalid in an URL. In general, they follow the same rules as .NET namespaces do.
         /// So Foo.Bar is a valid ID, Foo! and Foo Bar are not. 
         /// </summary>
-        public const string NuGetID = "PDFsharp";
+        public const string NuGetID = "";
 
         /// <summary>
         /// Nuspec Doc: The human-friendly title of the package displayed in the Manage NuGet Packages dialog.
         /// If none is specified, the ID is used instead. 
         /// </summary>
-        public const string NuGetTitle = "PDFsharp";
+        public const string NuGetTitle = "";
 
         /// <summary>
         /// Nuspec Doc: A comma-separated list of authors of the package code.
         /// </summary>
-        public const string NuGetAuthors = "empira Software GmbH";
+        public const string NuGetAuthors = "";
 
         /// <summary>
         /// Nuspec Doc: A comma-separated list of the package creators. This is often the same list as in authors.
         /// This is ignored when uploading the package to the NuGet.org Gallery. 
         /// </summary>
-        public const string NuGetOwners = "empira Software GmbH";
+        public const string NuGetOwners = "";
 
         /// <summary>
         /// Nuspec Doc: A long description of the package. This shows up in the right pane of the Add Package Dialog
         /// as well as in the Package Manager Console when listing packages using the Get-Package command. 
         /// </summary>
         // This assignment must be written in one line because it will be parsed from a PS1 file.
-        public const string NuGetDescription = "PDFsharp is the Open Source .NET library that easily creates and processes PDF documents on the fly from any .NET language. The same drawing routines can be used to create PDF documents, draw on the screen, or send output to any printer.";
+        public const string NuGetDescription = "";
 
         /// <summary>
         /// Nuspec Doc: A description of the changes made in each release of the package. This field only shows up
@@ -186,7 +186,7 @@ namespace PdfSharpCore
         /// Nuspec Doc: A short description of the package. If specified, this shows up in the middle pane of the
         /// Add Package Dialog. If not specified, a truncated version of the description is used instead.
         /// </summary>                  
-        public const string NuGetSummary = "A .NET library for processing PDF.";
+        public const string NuGetSummary = "";
 
         /// <summary>
         /// Nuspec Doc: The locale ID for the package, such as en-us.
@@ -200,18 +200,18 @@ namespace PdfSharpCore
         /// http://www.PdfSharpCore.net/NuGetPackage_PDFsharp-GDI.ashx
         /// http://www.PdfSharpCore.net/NuGetPackage_PDFsharp-WPF.ashx
         /// </remarks>
-        public const string NuGetProjectUrl = "https://www.nuget.org/packages/PdfSharpCore/";
+        public const string NuGetProjectUrl = "";
 
         /// <summary>
         /// Nuspec Doc: A URL for the image to use as the icon for the package in the Manage NuGet Packages
         /// dialog box. This should be a 32x32-pixel .png file that has a transparent background.
         /// </summary>
-        public const string NuGetIconUrl = "http://www.PdfSharp.net/resources/PDFsharp-Logo-32x32.png";
+        public const string NuGetIconUrl = "";
 
         /// <summary>
         /// Nuspec Doc: A link to the license that the package is under.
         /// </summary>                  
-        public const string NuGetLicenseUrl = "http://www.PdfSharpCore.net/PDFsharp_License.ashx";
+        public const string NuGetLicenseUrl = "";
 
         /// <summary>
         /// Nuspec Doc: A Boolean value that specifies whether the client needs to ensure that the package license (described by licenseUrl) is accepted before the package is installed.
@@ -222,7 +222,7 @@ namespace PdfSharpCore
         /// Nuspec Doc: A space-delimited list of tags and keywords that describe the package. This information is used to help make sure users can find the package using
         /// searches in the Add Package Reference dialog box or filtering in the Package Manager Console window.
         /// </summary>                  
-        public const string NuGetTags = "PDFsharp PDF creation";
+        public const string NuGetTags = "";
 
         /// <summary>
         /// The technology tag of the product:
@@ -234,6 +234,6 @@ namespace PdfSharpCore
         /// -wp : Windows Phone
         /// -wrt : Windows RunTime
         /// </summary>
-        public const string Technology = "-netstandard";
+        public const string Technology = "";
     }
 }
